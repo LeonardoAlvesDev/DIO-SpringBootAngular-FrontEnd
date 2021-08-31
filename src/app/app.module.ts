eedfe61a733b6007220ca41d661e8a972df82e32
LeonardoAlvesDev <leonaardo.alves@hotmail.com>
2021-08-31 20:03:22 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateRoomComponent } from './components/create-room/create-room.component';
+import { RoomDetailsComponent } from './components/room-details/room-details.component';
+import { RoomListComponent } from './components/room-list/room-list.component';
+import { UpdateRoomComponent } from './components/update-room/update-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRoomComponent,
+    RoomDetailsComponent,
+    RoomListComponent,
+    UpdateRoomComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
